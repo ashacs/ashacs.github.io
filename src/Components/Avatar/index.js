0880b841a428d35681da7fa3743e7959a141ae31
Asha Camper Singh <a.campersingh@gmail.com>
2019-02-12 21:46:01 -0800

    Added Avatar to Panel

---
+++ src/Components/Avatar/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './index.scss';
+
+class Avatar extends Component {
+
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <img class="rounded" src="/resources/avatar.jpg" height="200" width="200"/>
+        );
+    }
+
+};
+
+export default Avatar;
--- src/Components/Panel/index.js
+++ src/Components/Panel/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './index.scss';
 import Nav from '../Nav';
+import Avatar from '../Avatar';
 
 class Panel extends Component {
 
     render() {
         return (
             <div className="panel">
+                <Avatar />
                 <Nav />
             </div>
         );
