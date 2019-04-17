ed14d685de69a0af7f715cbfd194b750908d3bdc
Asha Camper Singh <acampers@ucsd.edu>
2019-04-16 21:17:42 -0700

    About page content

--- src/Components/Timeline/index.js
+++ src/Components/Timeline/index.js
@@ -13,12 +13,20 @@ class Timeline extends Component {
       <div className="timeline">
         <ul className="listBare timelineList">
           <TimelineEvent
-            title="My first event"
-            body="Information about my first event."
+            title="Sacramento"
+            body="Born and raised! Lady Bird really did capture everything you need to know."
           />
           <TimelineEvent
-            title="My second event"
-            body="Information about my second event."
+            title="San Diego"
+            body="UC San Diego class of 2016, Bachelors in Computer Science. While the sun was perpetually shining outside,
+              I spent my time especially interested in the theory of computation, discrete mathematics, 
+              and cryptography. "
+          />
+          <TimelineEvent
+            title="San Francisco"
+            body="From the sunniest place in California, to the foggiest place in California. I entered the 'real world'
+              (though I would say all those school assignments felt pretty darn real), starting a full-time 
+              software engineering role at Salesforce."
           />
         </ul>
       </div>
