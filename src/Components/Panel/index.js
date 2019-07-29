bea5b5a235e555b687d50664c7347953c27cd296
Asha Camper Singh <acampers@ucsd.edu>
2019-07-28 21:25:45 -0700

    Updated font, colors, title, and timeline content.

--- src/Components/Panel/index.js
+++ src/Components/Panel/index.js
@@ -7,6 +7,7 @@ class Panel extends Component {
   render() {
     return (
       <div className="panel">
+        <h1 className="panelTitle">Asha C.S.</h1>
         <Avatar />
         <Nav navItemsList={["about", "career", "interests"]} />
       </div>
--- src/Components/Timeline/index.js
+++ src/Components/Timeline/index.js
@@ -18,15 +18,14 @@ class Timeline extends Component {
           />
           <TimelineEvent
             title="San Diego"
-            body="UC San Diego class of 2016, with a Bachelors in Computer Science. While the sun was perpetually 
-              shining outside, I spent my time especially interested in the theory of computation, discrete mathematics, 
-              and cryptography. "
+            body="UC San Diego class of 2016, with a Bachelors in Computer Science. While the sun was 
+              shining outside, I spent my time especially interested in the computational theory, discrete 
+              mathematics, and cryptography. "
           />
           <TimelineEvent
             title="San Francisco"
-            body="From the sunniest place in California, to the foggiest place in California. I entered the 'real world'
-              (though I would say all those school assignments felt pretty darn real), starting a full-time 
-              software engineering role at Salesforce."
+            body="From the sunniest place in California, to the foggiest place in California. I entered the 
+            software engineering industry, starting a full-time developer position at Salesforce."
           />
         </ul>
       </div>
