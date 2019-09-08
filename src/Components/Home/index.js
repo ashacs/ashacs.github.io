76ac187807d37f38397b20511f9a82c7acf289ae
Asha Camper Singh <acampers@ucsd.edu>
2019-09-07 21:12:16 -0700

    Add home page blurb

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -4,8 +4,11 @@ import "./index.scss";
 class Home extends Component {
   render() {
     return (
-      <div className="page-content">
-        <h1>hi</h1>
+      <div className="page-content home">
+        <p className="home-blurb">
+          Hi, my name is <span className="home-blurb-emphasis">Asha</span>! I'm
+          a software engineer by day and a puzzle-doer (puzzler?) by night.
+        </p>
       </div>
     );
   }
