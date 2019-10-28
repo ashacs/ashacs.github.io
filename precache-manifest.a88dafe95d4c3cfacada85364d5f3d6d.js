f534c481da72076ccc00effe766b1d349517c2b9
Asha Camper Singh <acampers@ucsd.edu>
2019-10-27 22:28:25 -0700

    Updates

--- precache-manifest.a88dafe95d4c3cfacada85364d5f3d6d.js
+++ precache-manifest.a88dafe95d4c3cfacada85364d5f3d6d.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "05ebec64c32c02c75167",
+    "revision": "bd9c8c58f0de237c1abe",
     "url": "/static/css/main.3f3390ff.chunk.css"
   },
   {
-    "revision": "05ebec64c32c02c75167",
-    "url": "/static/js/main.05ebec64.chunk.js"
+    "revision": "bd9c8c58f0de237c1abe",
+    "url": "/static/js/main.bd9c8c58.chunk.js"
   },
   {
     "revision": "e8a73c1185417231402c",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "7ce31eb75e0d9abaec5fdb40606a2a9d",
+    "revision": "40662902a244518fe5634e17e2a5435b",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1fbea26ee43d20819a969502fef892e5.js"
+  "/precache-manifest.a88dafe95d4c3cfacada85364d5f3d6d.js"
 );
 
 workbox.clientsClaim();
