80b25bca69ddf24630e23f3fc8b591c5dff2e3ed
Asha Camper Singh <acampersingh@gmail.com>
2020-04-18 18:32:07 -0700

    Make timeline re-useable

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -2,16 +2,45 @@ import React, { Component } from "react";
 import "./index.scss";
 import Timeline from "../Timeline";
 
+
 class About extends Component {
   constructor() {
     super();
     this.state = {};
   }
 
+  events = [
+    {
+      title: "Sacramento",
+      body: "Born and raised! Lady Bird really did capture everything you need to know."
+    },
+    {
+      title: "San Diego",
+      body: "UC San Diego class of 2016, with a Bachelors in Computer Science. While the sun was shining outside, I spent my time especially interested in computational theory, discrete mathematics, and cryptography. "
+    },
+    {
+      title: "San Francisco",
+      body: "From the sunniest place in California, to the foggiest place in California. I entered the software engineering industry, starting a full-time developer position at Salesforce."
+    }];
+
+  renderHeader() {
+    return (
+      <h1>
+        <span role="img" aria-label="Map emoji">
+          🗺️
+          </span>{" "}
+          My Journey
+      </h1>
+    )
+  }
+
   render() {
     return (
       <div className="page-content ">
-        <Timeline />
+        <Timeline
+          header={this.renderHeader()}
+          events={this.events}
+        />
       </div>
     );
   }
--- src/Components/Timeline/index.js
+++ src/Components/Timeline/index.js
@@ -8,32 +8,25 @@ class Timeline extends Component {
     this.state = {};
   }
 
+  renderEvents() {
+    return (
+      <ul className="listBare timelineList">
+        {this.props.events.map((event) => (
+          <TimelineEvent
+            key={event.title}
+            title={event.title}
+            body={event.body}
+          />
+        ))}
+      </ul>
+    )
+  }
+
   render() {
     return (
       <div className="timeline">
-        <h1>
-          <span role="img" aria-label="Map emoji">
-            🗺️
-          </span>{" "}
-          My Journey
-        </h1>
-        <ul className="listBare timelineList">
-          <TimelineEvent
-            title="Sacramento"
-            body="Born and raised! Lady Bird really did capture everything you need to know."
-          />
-          <TimelineEvent
-            title="San Diego"
-            body="UC San Diego class of 2016, with a Bachelors in Computer Science. While the sun was 
-              shining outside, I spent my time especially interested in computational theory, discrete 
-              mathematics, and cryptography. "
-          />
-          <TimelineEvent
-            title="San Francisco"
-            body="From the sunniest place in California, to the foggiest place in California. I entered the 
-            software engineering industry, starting a full-time developer position at Salesforce."
-          />
-        </ul>
+        {this.props.header}
+        {this.renderEvents()}
       </div>
     );
   }
