5b6047dda42495604bde528c549effaaa6d795ab
Asha Camper Singh <acampersingh@gmail.com>
2020-04-18 20:16:35 -0700

    Fix typo

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -16,7 +16,7 @@ class About extends Component {
     },
     {
       title: "San Diego",
-      body: "I graduaced from UC San Diego with a Bachelors in Computer Science. While the sun was shining outside, I spent my time especially interested in computational theory, discrete mathematics, and cryptography. "
+      body: "I graduated from UC San Diego with a Bachelors in Computer Science. While the sun was shining outside, I spent my time especially interested in computational theory, discrete mathematics, and cryptography. "
     },
     {
       title: "San Francisco",
