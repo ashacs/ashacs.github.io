94fdef44dd78eba5d1534fd2d4d6718e326d949c
Asha Camper Singh <acampersingh@gmail.com>
2020-06-10 18:51:04 -0700

    Refactor interests to iterate over list, and update currently reading

--- src/Components/Interests/index.js
+++ src/Components/Interests/index.js
@@ -8,31 +8,43 @@ class Interests extends Component {
     this.state = {};
   }
 
+  interests = [
+    {
+      title: "Listening",
+      items: [
+        {
+          title: "Reply All (podcast)",
+          link: "https://gimletmedia.com/shows/reply-all"
+        },
+        {
+          title: 'Tennis (band)',
+          link: "https://open.spotify.com/artist/1ybAN3utgdoUL1MUCtH4QM"
+        },
+        {
+          title: 'Papooz (band)',
+          link: 'https://open.spotify.com/artist/2gaCpvhi31OZtozJKsr5gm'
+        }
+      ]
+    },
+    {
+      title: "Watching",
+      items: [{ title: "The West Wing" }, { title: "Seinfeld" }, { title: "Buffy the Vampire Slayer" }]
+    },
+    {
+      title: "Reading",
+      items: [{ title: "Stony the Road: Reconstruction, White Supremacy, and the Rise of Jim Crow" }]
+    }
+  ];
+
   render() {
     return (
       <div className="page-content interests">
-        <Card
-          title="Listening"
-          items={[
-            {
-              title: "Reply All (podcast)",
-              link: "https://gimletmedia.com/shows/reply-all"
-            },
-            {
-              title: 'Tennis (band)',
-              link: "https://open.spotify.com/artist/1ybAN3utgdoUL1MUCtH4QM"
-            },
-            {
-              title: 'Papooz (band)',
-              link: 'https://open.spotify.com/artist/2gaCpvhi31OZtozJKsr5gm'
-            }
-          ]}
-        />
-        <Card
-          title="Watching"
-          items={[{ title: "The West Wing" }, { title: "Seinfeld" }, { title: "Buffy the Vampire Slayer" }]}
-        />
-        <Card title="Reading" items={[{ title: "Song of Solomon" }]} />
+        {this.interests.map((interest) => (
+            <Card
+              title={interest.title}
+              items={interest.items}
+            />
+        ))}        
       </div>
     );
   }
