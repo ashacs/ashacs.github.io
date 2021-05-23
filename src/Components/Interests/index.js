761e14cb520901e60613c081fd43e2ec626c50b6
Asha Camper Singh <acampersingh@gmail.com>
2021-05-23 16:26:08 -0700

    Update interests

--- src/Components/Interests/index.js
+++ src/Components/Interests/index.js
@@ -13,46 +13,54 @@ class Interests extends Component {
       title: "Listening",
       items: [
         {
-          title: "Reply All (podcast)",
-          link: "https://gimletmedia.com/shows/reply-all"
+          title: "Coffee Break Italian (podcast)",
+          link: "https://coffeebreaklanguages.com/coffeebreakitalian/",
         },
         {
           title: "Serial - Season 3 (podcast)",
-          link: "https://serialpodcast.org/season-three/about"
+          link: "https://serialpodcast.org/season-three/about",
         },
         {
-          title: 'Tennis (band)',
-          link: "https://open.spotify.com/artist/1ybAN3utgdoUL1MUCtH4QM"
+          title: "Anderson .Paak",
+          link: "https://open.spotify.com/artist/3jK9MiCrA42lLAdMGUZpwa",
         },
         {
-          title: 'Papooz (band)',
-          link: 'https://open.spotify.com/artist/2gaCpvhi31OZtozJKsr5gm'
-        }
-      ]
+          title: "Vulfpeck",
+          link: "https://open.spotify.com/artist/7pXu47GoqSYRajmBCjxdD6",
+        },
+        {
+          title: "St. Vincent",
+          link: "https://open.spotify.com/artist/7bcbShaqKdcyjnmv4Ix8j6",
+        },
+      ],
     },
     {
       title: "Watching",
-      items: [{ title: "The West Wing" }, { title: "Buffy the Vampire Slayer" }]
+      items: [{ title: "The Handmaid's Tale" }, { title: "Mr. Robot" }],
     },
     {
       title: "Reading",
-      items: [{ 
-        title: "Sapiens by Yuval Noah Harari", 
-        link: "https://www.goodreads.com/book/show/23692271-sapiens" 
-      }]
-    }
+      items: [
+        {
+          title:
+            "The Righteous Mind: Why Good People Are Divided by Politics and Religion by Jonathan Haidt",
+          link:
+            "https://www.goodreads.com/book/show/11324722-the-righteous-mind",
+        },
+      ],
+    },
   ];
 
   render() {
     return (
       <div className="page-content interests">
         {this.interests.map((interest) => (
-            <Card
-              key={interest.title}
-              title={interest.title}
-              items={interest.items}
-            />
-        ))}        
+          <Card
+            key={interest.title}
+            title={interest.title}
+            items={interest.items}
+          />
+        ))}
       </div>
     );
   }
