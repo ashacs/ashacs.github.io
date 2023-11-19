973061c0e4de25e36888f60c339701698bf01ca7
Asha Camper Singh <acampersingh@gmail.com>
2023-11-19 14:25:31 -0800

    Display maintenance page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from "react";
 import "./App.scss";
-import Panel from "./Components/Panel";
-import About from "./Components/About";
-import Career from "./Components/Career";
-import Interests from "./Components/Interests";
-import Home from "./Components/Home";
-import Footer from "./Components/Footer";
+import { Maintenance } from "./Components/Maintenance";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <Router basename={process.env.PUBLIC_URL}>
-        <div className="App">
-          <Panel />
-          <Route path="/" exact={true} component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/career" component={Career} />
-          <Route path="/interests" component={Interests} />
-          <span className="app-footer">
-            <Footer />
-          </span>
-        </div>
-      </Router>
-    );
-  }
+export default function App() {
+  return <Maintenance />;
 }
-
-export default App;
---
+++ src/Components/Maintenance/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.scss";
+
+export function Maintenance() {
+  return (
+    <div className="maintenance-container">
+      <h1 className="maintenance-title">Coming soon</h1>
+      <p className="maintenance-subtitle">to a browser near you</p>
+    </div>
+  );
+}
