91075b966a4fa013bf7729fd32bc9e27f4c9373d
Asha Camper Singh <acampersingh@gmail.com>
2024-07-28 18:29:01 -0700

    Add dark and light mode theme svgs

---
+++ src/components/SVG/DarkThemeSVG.tsx
@@ -0,0 +1,14 @@
+export function DarkThemeSVG() {
+  return (
+    <svg
+      fill="currentColor"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+    >
+      <path d="M0 12c0 6.627 5.373 12 12 12s12-5.373 12-12-5.373-12-12-12-12 5.373-12 12zm2 0c0-5.514 4.486-10 10-10v20c-5.514 0-10-4.486-10-10z" />
+    </svg>
+  );
+}
