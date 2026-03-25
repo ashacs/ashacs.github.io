baa80ab48406d7f2838645de47175b8f32a9d019
Asha Camper Singh <acampersingh@gmail.com>
2026-03-24 22:35:48 -0700

    refactor: new anecdote component

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -5,35 +5,37 @@ import { Skills } from '../Skills';
 import { Values } from '../Values';
 import { SectionTitle } from '../SectionTitle';
 import './index.css';
+import Anecdote from '../Anecdote';
 
 export function Home() {
   return (
     <div className="home-container">
-      <section className="authors-note">
-        <SectionTitle>Author&apos;s Note</SectionTitle>
-        <p className="authors-note-text">
-          Welcome to my personal website. I&apos;m a frontend-focused developer
-          with a desire to work alongside thoughtful teammates on high-quality
+      <section>
+        <SectionTitle>Authorʼs Note</SectionTitle>
+        <Anecdote>
+          Welcome to my personal website. Iʼm a frontend-focused developer with
+          a desire to work alongside thoughtful teammates on high-quality
           products.
-        </p>
+        </Anecdote>
       </section>
 
-      <section className="experience">
+      <section>
         <SectionTitle>Experience</SectionTitle>
         <Experience />
       </section>
 
-      <section className="about-the-author">
+      <section>
         <SectionTitle>About the Author</SectionTitle>
         <Skills />
         <Values />
       </section>
 
-      <section className="appendix">
+      <section>
         <SectionTitle>Appendix</SectionTitle>
         <Awards />
         <Education />
       </section>
+
     </div>
   );
 }
